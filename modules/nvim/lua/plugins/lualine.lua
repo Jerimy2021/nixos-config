6ee7d78a65fc6d589442cc5ac29bab1a1dd9ace2
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2026-06-07 08:34:28 -0500

    feat: colores actualizados de neovim cyberdream

--- modules/nvim/lua/plugins/lualine.lua
+++ modules/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@
 require('lualine').setup({
   options = {
     -- Cámbialo a: 'gruvbox', 'tokyonight', o 'auto' (para cyberdream)
-    theme = 'cyberdream', 
+    theme = 'auto', 
     component_separators = { left = '│', right = '│' },
     section_separators = { left = '', right = '' },
     globalstatus = true,
